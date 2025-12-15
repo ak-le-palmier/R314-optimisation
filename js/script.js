a7d534f7be7edeef09b8c0a43e07b0a27a134901
Ak <ak.ou.akela@gmail.com>
2025-12-15 11:34:34 +0100

    compression image et ajout au code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-(function(){
+/*(function(){
   window.addEventListener('load', function(){
     const imgs = document.querySelectorAll('.card img');
     imgs.forEach(img => { if (img.complete) img.classList.add('loaded'); else img.addEventListener('load', ()=> img.classList.add('loaded')); });
   });
-})();
+})();*/
